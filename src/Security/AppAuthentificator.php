ad658fbffeea986e035d2e337eee7ffa447cf395
DubDubToinou <dubuc.ant@gmail.com>
2022-10-18 11:20:00 +0200

    Ajout form registration + remember me

--- src/Security/AppAuthentificator.php
+++ src/Security/AppAuthentificator.php
@@ -48,8 +48,7 @@ class AppAuthentificator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('app_main'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
