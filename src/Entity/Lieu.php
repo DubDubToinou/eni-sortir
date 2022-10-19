7b3042c7683e01ecba0b4936a82de7602716bc67
DubDubToinou <dubuc.ant@gmail.com>
2022-10-19 16:33:05 +0200

    retrait de la longitude et latitude dans l'entity Lieu

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -68,30 +68,6 @@ class Lieu
         return $this;
     }
 
-    public function getLatitude(): ?float
-    {
-        return $this->latitude;
-    }
-
-    public function setLatitude(float $latitude): self
-    {
-        $this->latitude = $latitude;
-
-        return $this;
-    }
-
-    public function getLongitude(): ?float
-    {
-        return $this->longitude;
-    }
-
-    public function setLongitude(float $longitude): self
-    {
-        $this->longitude = $longitude;
-
-        return $this;
-    }
-
     /**
      * @return Collection<int, Sortie>
      */
