c4ac84313ab25e92986ed33a44b363628b4446c3
DubDubToinou <dubuc.ant@gmail.com>
2022-10-19 11:12:57 +0200

    Ajout au panel admin. suppression / désactivation d'un utilisateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Repository\ParticipantRepository;
+use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,10 +33,41 @@ class AdminController extends AbstractController
     public function indexUtilisateurNonActif(ParticipantRepository $participantRepository): Response
     {
         $utilisateursActif = $participantRepository->afficherTousLesutilisateursInactifs();
-        dump($utilisateursActif);
         return $this->render('admin/gererUtilisateurNonActif.html.twig', [
             'utilisateursActif'=>$utilisateursActif
         ]);
+    }
+
+    #[Route('/supprimerUtilisateur/{id}', name: 'admin_supprimer_utilisateur')]
+    public function supprimerUtilisateur(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $participantASupprimer = $participantRepository->find($id);
+
+        $participantRepository->remove($participantASupprimer, true);
+
+        return $this->redirectToRoute('admin_gerer_utilisateur');
+    }
+
+    #[Route('/desactiverUtilisateur/{id}', name: 'admin_desactiver_utilisateur')]
+    public function desactiverUtilisateur(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $participantADesactiver = $participantRepository->find($id);
+        $participantADesactiver->setActif(0);
+        $participantRepository->save($participantADesactiver, true);
+
+
+        return $this->redirectToRoute('admin_gerer_utilisateur');
+    }
+
+    #[Route('/activerUtilisateur/{id}', name: 'admin_activer_utilisateur')]
+    public function activerUtilisateur(int $id, ParticipantRepository $participantRepository): Response
+    {
+        $participantAActiver = $participantRepository->find($id);
+        $participantAActiver->setActif(1);
+        $participantRepository->save($participantAActiver, true);
+
+
+        return $this->redirectToRoute('admin_gerer_utilisateur_non_actif');
     }
 
 
