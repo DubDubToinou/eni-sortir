0c0c70de0d0da576b57863cf03afdd1b01413761
afkus3r <sheila.skalli@gmail.com>
2022-10-20 14:22:24 +0200

    Fix dynamic new sortie form

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -3,15 +3,21 @@
 namespace App\Form;
 
 use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SortieType extends AbstractType
 {
@@ -33,13 +39,28 @@ class SortieType extends AbstractType
             ->add('nbInscriptionsMax', IntegerType::class, [
                 'label' => 'Nombre de places'
             ])
-            ->add('infosSortie', TextType::class, [
+            ->add('infosSortie', TextAreaType::class, [
                 'label' => 'Description'
             ])
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
-                'choice_label' => 'nom'
+            ->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'mapped' => false,
+                'required' => false
             ]);
+
+        $builder->get('ville')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $form->getParent()->add('lieu', EntityType::class, [
+                    'class' => Lieu::class,
+                    'mapped' => true,
+                    'required' => true,
+                    'choices' => $form->getData()->getLieux()
+                ]);
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
