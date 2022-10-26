6394df0fe74d46a5710f50eda076321abfda5f4e
DubDubToinou <dubuc.ant@gmail.com>
2022-10-26 10:00:25 +0200

    Ajout block javascript.

--- src/Form/SortieModifierType.php
+++ src/Form/SortieModifierType.php
@@ -9,12 +9,16 @@ use App\Repository\VilleRepository;
 use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Test\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieModifierType extends AbstractType
@@ -69,27 +73,38 @@ class SortieModifierType extends AbstractType
                 },
                 'required'=>false,
                 'mapped'=>false,
-                'attr'=>[
-                    'id'=>'ville',
-                ]
             ])
-            ->add('lieu', EntityType::class,[
-                'class'=>Lieu::class,
-                'label'=>'Lieu ',
-                'placeholder'=>'-- Selectionner la ville --',
-                'choice_label'=>'nom',
-                'attr'=>[
-                    'id'=>'lieu'
-                ]
-            ])
+            ->add('lieu', ChoiceType::class, [
+                'placeholder'=>'-- Choissisez un lieu --'
+            ]);
+
+
+
+            $formModifier = function (FormInterface $form, Ville $ville = null){
+                $lieu = (null === $ville) ? [] : $ville->getLieux();
+                $form->add('lieu', EntityType::class,[
+                    'class' => Lieu::class,
+                    'choices' => $lieu,
+                    'choice_label' => 'nom',
+                    'placeholder' => 'lieu [choisir une ville]',
+                    'label' => "Lieu : "
+                ]);
+            };
+
+        $builder->get('ville')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $eventVille) use ($formModifier){
+                $ville = $eventVille->getForm()->getData();
+                $formModifier($eventVille->getForm()->getParent(), $ville);
+            }
+        );
+    }
 
             //->add('organisateur')
             //->add('participants')
             //->add('etat')
             //->add('lieu')
             //->add('campus')
-        ;
-    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
